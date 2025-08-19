dc55e7f45d1b2f8b453866a8df5d9ecea0467096
samiran <sam.de.721166@gmail.com>
2025-08-20 00:14:21 +0530

    Add files via upload

---
+++ model/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://127.0.0.1:27017/auth_app")
+
+
+const todoModel = mongoose.Schema({
+    title: String,
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+module.exports = mongoose.model("user", todoModel)
